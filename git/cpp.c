47d45e84d92cee3287e9448075b3b9964a682f77
Phulkeshwar Mahto <pkmahto009@gmail.com>
2024-12-19 00:09:55 +0530

    he

--- git/cpp.c
+++ git/cpp.c
@@ -3,4 +3,7 @@ int main(){
       printf("%d");
       printf("JJ thompson");
       //this is comment for checking purpose 
+      for (int i =2; i=44;i++){
+            printf("hello");
+      }
 }
